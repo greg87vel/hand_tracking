5085b184a27def8a9564fa83044ad38aee484f47
Gregory Specchi <gregory.specchi@unikey.it>
2023-09-20 11:51:45 +0200

    aggiunto hands-game

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -22,29 +22,39 @@ class HandDetector:
 
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(imgRGB)
-
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
+        self.results = self.hands.process(imgRGB)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
-                    '''for id, lm in enumerate(handLms.landmark):
-                        h, w, c = img.shape
-                        cx, cy = int(lm.x * w), int(lm.y * h)
-                        print(id, cx, cy)
-                        if id in [4, 8, 12, 16, 20]:
-                            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)'''
         return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            hand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(hand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                # print(id, cx, cy)
+                lmList.append([id, cx, cy])
+                if draw:
+                    if id in [4, 8, 12, 16, 20]:
+                        cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+        return lmList
 
 
 def main():
     pTime = 0
-    cTime = 0
     cap = cv2.VideoCapture(0)
-    detector=HandDetector()
+    detector = HandDetector()
     while True:
         success, img = cap.read()
         img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[8])
+
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
